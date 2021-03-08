83036fb7bffde436e676428e571e634fee867239
admin_zt <admin_zt@ZT.MADISON.COM>
2021-03-08 12:52:00 -0500

    fix ui

--- src/components/AboutSection/AboutSection.svelte
+++ src/components/AboutSection/AboutSection.svelte
@@ -1,10 +1,12 @@
 <section class="about-section">
-    <img src="/imgs/aboutus.jpg" alt="About Madison">
-    <div class="about">
-        <h1>About Us</h1>
-        <p>
-            Here at Madison, we pride ourselves on serving food that delivers the most exquisite pleasure to your taste buds and is healthy for your body. We are Ridgewood’s first sous vide themed restaurant concept and when it comes to serving healthy meals, Ridgewood’s  residents have counted on us time and time again.
-        </p>    
+    <div class="container">
+        <img src="/imgs/aboutus.jpg" alt="About Madison">
+        <div class="about">
+            <h1>About Us</h1>
+            <p>
+                Here at Madison, we pride ourselves on serving food that delivers the most exquisite pleasure to your taste buds and is healthy for your body. We are Ridgewood’s first sous vide themed restaurant concept and when it comes to serving healthy meals, Ridgewood’s  residents have counted on us time and time again.
+            </p>    
+        </div>
     </div>
     
 </section>
