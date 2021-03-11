75e97d554029ba7f40497e1074d17b8fb94070c9
admin_zt <admin_zt@ZT.MADISON.COM>
2021-03-11 13:09:40 -0500

    update ui scroll

--- src/client.js
+++ src/client.js
@@ -14,7 +14,7 @@ let body = document.querySelector('body'),
 observerPopup = new MutationObserver(mCallbackPopup);
 
 
-function mCallbackPopup(mutations) {
+async function mCallbackPopup(mutations) {
   for (let mutation of mutations) {
     if (mutation.type === 'childList') {
 		let node = mutation.addedNodes && mutation.addedNodes[0]
@@ -28,7 +28,25 @@ function mCallbackPopup(mutations) {
         }
 			  node.style.top = top
 			  node.style.maxHeight = maxHeight
-		}
+
+        if(window.innerWidth < 600) return
+        await new Promise(r => setTimeout(r,300))
+        
+        let cartHasNoImg = node.querySelector('.product-details--no-images')
+        let embedContent = node.querySelector('.ecwid-ProductBrowserPopup-content')
+        if(embedContent){
+          let nw = embedContent.firstElementChild.firstElementChild
+          if(cartHasNoImg){
+            embedContent.style.width = "100%"
+            nw.style.width = '300px'
+          }else{
+            embedContent.style.width = "80vw"
+            nw.style.width = 'inherit'
+            nw.setAttribute('class','ec-size ec-size--xxs ec-size--xs ec-size--s ec-size--m ecwid-no-touch ecwid-supports-cssanimations ecwid-responsive ecwid-no-dragndrop')
+          }
+        }
+
+		  }
     }
   }
 }
