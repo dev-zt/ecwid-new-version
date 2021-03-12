eeffce97d08e2c8d8f6fb00f5aa9105915445410
admin_zt <admin_zt@ZT.MADISON.COM>
2021-03-12 06:33:10 -0500

    update ui

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -40,22 +40,24 @@
 
 </script>
 <section class="navbar" style={$navShadow ? 'box-shadow: var(--shadow-md);':''}>
-    <nav class:open={!close}>
-        <div id="mobile-menu" class="m2" class:hide={!close}>
+    <nav >
+        <div id="mobile-menu" >
             <div data-layout="SMALL_ICON" class="ec-cart-widget"></div>
         </div>
-        <a href="/" class="logo" on:click|stopPropagation class:open={!close}>
-            <img src="/logo-192.png" alt="madison logo" >
-        </a>   
-        <i class="fas fa-bars bars icon" class:hide={!close} on:click={openMenu} />
-        <div id="menu" class="menu" class:close>
-            <i class="fas fa-times icon" on:click={closeMenu}/>
-            <div class="container">
-                <a class="start" on:click|stopPropagation href="/#cat-nav" class:selected={segment==""}> Menu </a>
-                <a href="/contact" on:click|stopPropagation class:selected={segment=="contact"}> Contact </a>
-                {#if canDownload}
-                    <div class="link" on:click={installPWA}>Download App</div>
-                {/if}
+        <div class="nav-s1" class:open={!close}>
+            <a href="/" class="logo" on:click|stopPropagation class:open={!close}>
+                <img src="/logo-192.png" alt="madison logo" >
+            </a>   
+            <i class="fas fa-bars bars icon" class:hide={!close} on:click={openMenu} />
+            <div id="menu" class="menu" class:close>
+                <img alt="close" class="icon" src="/icons/close.svg" on:click={closeMenu} />
+                <div class="container">
+                    <a class="start" on:click|stopPropagation href="/#cat-nav" class:selected={segment==""}> Menu </a>
+                    <a href="/contact" on:click|stopPropagation class:selected={segment=="contact"}> Contact </a>
+                    {#if canDownload}
+                        <div class="link" on:click={installPWA}>Download App</div>
+                    {/if}
+                </div>
             </div>
         </div>
     </nav>
