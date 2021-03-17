1800cce379a30701c6a7220eeb3f739a1151232f
admin_zt <admin_zt@ZT.MADISON.COM>
2021-03-17 11:20:44 -0400

    change about section

--- src/components/AboutSection/AboutSection.svelte
+++ src/components/AboutSection/AboutSection.svelte
@@ -1,11 +1,15 @@
 <section class="about-section">
     <div class="container">
-        <img src="/imgs/aboutus.jpg" alt="About Madison">
+        <h1>About Madison Grill</h1>
         <div class="about">
-            <h1>About Us</h1>
             <p>
                 Here at Madison, we pride ourselves on serving food that delivers the most exquisite pleasure to your taste buds and is healthy for your body. We are Ridgewood’s first sous vide themed restaurant concept and when it comes to serving healthy meals, Ridgewood’s  residents have counted on us time and time again.
             </p>    
+            <div class="avatar">
+                <img src="https://randomuser.me/api/portraits/women/68.jpg" alt="About Seco Farm">
+                <div>Mary Smith</div>
+                <div>Madison Grill Shop owner</div>
+            </div>
         </div>
     </div>
     
